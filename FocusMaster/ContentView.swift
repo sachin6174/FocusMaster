febb0f203f07b71fa6155f49348ac3a6de603f4a
sachin kumar <sachinmehtab@gmail.com>
2025-03-08 02:24:44 +0530

    Initial Commit

---
+++ FocusMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusMaster
+//
+//  Created by sachin kumar on 08/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusMaster/FocusMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusMasterApp.swift
+//  FocusMaster
+//
+//  Created by sachin kumar on 08/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
